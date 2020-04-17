0f0a09b69494ba025abcfd7f93070fc726ea40ee
rom-23 <romain.laurent23@gmail.com>
2020-04-17 16:06:25 +0200

    rom commit admin JS

---
+++ public/js/admin/multiselect.js
@@ -0,0 +1,24 @@
+﻿import MultiSelect from "../multi-select2/multi-select.js";
+
+const multiSelect = (function () {
+    const route = '/admin/apiTag';
+    const createComponent = async function () {
+        try {
+            let response = await fetch(route);
+            if (response.ok) {
+                let data = await response.json();
+                let component = new MultiSelect('.multi-select', {
+                    items: JSON.parse(data)
+                });
+                component.on('change', function (e) {
+                    console.log(e.detail, component.getCurrent('id'));
+                });
+                return component;
+            }
+        } catch (e) {
+            alert(e.message);
+        }
+    };
+    return createComponent();
+})();
+export default multiSelect;
